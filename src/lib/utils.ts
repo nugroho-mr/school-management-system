d2a54189302926b4f4e53a221f9c013d8cd70aa1
Panca Satrio Nugroho <nugroho.panca@gmail.com>
2026-01-05 10:37:51 +0700

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,15 +4,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-import { useGlobalProcessing } from './store/useGlobalProcessing'
-
-export async function runWithGlobalProcessing<T>(fn: () => Promise<T>) {
-  const { show, hide } = useGlobalProcessing.getState()
-  show()
-  try {
-    return await fn()
-  } finally {
-    hide()
-  }
-}
